c43ec1643587f127b463a57fd3d43cd99af7e69c
wooji-dev <wooji.dev@gmail.com>
2026-03-04 14:52:14 +0900

    Feat: 사이드바 고정

---
+++ src/app/(group)/layout.tsx
@@ -0,0 +1,23 @@
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import Sidebar from "../../components/dashboard/sidebar";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (!session) {
+    redirect("/login"); // 로그인 안 했으면 로그인 페이지로!
+  }
+
+  return (
+    <div className="flex">
+      <Sidebar user={session.user} />
+      <main className="flex-1 p-8">{children}</main>
+    </div>
+  );
+}
