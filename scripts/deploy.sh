4c34e9c569616ca4a60fa8a11d9c88658d126a29
sungbeen park <psb1105a@gmail.com>
2026-03-05 18:00:11 +0900

    배포 성공 후 수정 deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,12 +5,16 @@ echo ">>> 배포 시작" >> /home/ubuntu/deploy.log
 cd /home/ubuntu/app
 
 echo ">>> 의존성 설치" >> /home/ubuntu/deploy.log
-npm install --production >> /home/ubuntu/deploy.log 2>> /home/ubuntu/deploy_err.log
+npm install >> /home/ubuntu/deploy.log 2>> /home/ubuntu/deploy_err.log
+
+echo ">>> 빌드" >> /home/ubuntu/deploy.log
+npm run build >> /home/ubuntu/deploy.log 2>> /home/ubuntu/deploy_err.log
 
 echo ">>> 현재 실행중인 애플리케이션 종료" >> /home/ubuntu/deploy.log
-sudo ps -ef | grep node | grep -v grep | awk '{print $2}' | xargs kill -15 2>/dev/null || true
+pm2 delete solvps 2>/dev/null || true
 
 echo ">>> 애플리케이션 실행" >> /home/ubuntu/deploy.log
-nohup node app.js >> /home/ubuntu/deploy.log 2>> /home/ubuntu/deploy_err.log &
+pm2 start npm --name solvps -- start >> /home/ubuntu/deploy.log 2>> /home/ubuntu/deploy_err.log
+pm2 save >> /home/ubuntu/deploy.log
 
 echo ">>> 배포 완료" >> /home/ubuntu/deploy.log
